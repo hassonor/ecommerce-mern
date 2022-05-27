ddd06a35c78b14929b180a3356ad5d85f5310619
Or Hasson <hassonor@gmail.com>
2022-05-27 08:31:50 +0300

    Adding Update user profile screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {
+    BrowserRouter, Link, Route, Routes,
+} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
@@ -10,6 +10,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
 import {useContext} from 'react';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 import {Store} from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -17,8 +19,9 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
-import OrderScreen from "./screens/OrderScreen";
-import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import OrderScreen from './screens/OrderScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
     const {state, dispatch: ctxDispatch} = useContext(Store);
@@ -29,6 +32,7 @@ function App() {
         localStorage.removeItem('userInfo');
         localStorage.removeItem('shippingAddress');
         localStorage.removeItem('paymentMethod');
+        window.location.href = '/signin';
     };
     return (
         <BrowserRouter>
@@ -80,19 +84,21 @@ function App() {
                 </header>
                 <main>
                     <Container className="mt-3">
+                        {/* eslint-disable-next-line react/react-in-jsx-scope */}
                         <Routes>
                             <Route path="/product/:slug" element={<ProductScreen/>}/>
                             <Route path="/cart" element={<CartScreen/>}/>
                             <Route path="/signin" element={<SigninScreen/>}/>
                             <Route path="/signup" element={<SignupScreen/>}/>
+                            <Route path="/profile" element={<ProfileScreen/>}/>
                             <Route path="/placeorder" element={<PlaceOrderScreen/>}/>
                             <Route path="/order/:id" element={<OrderScreen/>}/>
                             <Route path="/orderhistory" element={<OrderHistoryScreen/>}/>
                             <Route
                                 path="/shipping"
                                 element={<ShippingAddressScreen/>}
-                            ></Route>
-                            <Route path="/payment" element={<PaymentMethodScreen/>}></Route>
+                            />
+                            <Route path="/payment" element={<PaymentMethodScreen/>}/>
                             <Route path="/" element={<HomeScreen/>}/>
                         </Routes>
                     </Container>
